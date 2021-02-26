6478ae97c13f87fe52b58938868838e342f3821a
Sam Phillips <samdphillips@gmail.com>
2021-02-26 14:12:58 -0800

    Tidy up raise-backend-error function

--- keyring-lib/keyring/private/error.rkt
+++ keyring-lib/keyring/private/error.rkt
@@ -16,15 +16,15 @@
 
 (define (raise-backend-error who msg [backend #f] [details null])
   (define full-message
-    (~a (~.a who ": " msg ";") "\n"
-         (apply ~a #:separator "\n"
-                (~.a "  backend: " backend)
-                (for/list ([kv (in-list details)])
-                  (~.a "  " (car kv) ": " (cdr kv))))))
+    (apply ~a #:separator "\n"
+      (~.a who ": " msg ";")
+      (~.a "  backend: " backend)
+      (for/list ([kv (in-list details)])
+        (~.a "  " (car kv) ": " (cdr kv)))))
   (raise
     (keyring-backend-error full-message
-                                (current-continuation-marks)
-                                backend)))
+                           (current-continuation-marks)
+                           backend)))
 
 (define (raise-backend-load-error who msg [backend #f] [details null])
   (define full-message
