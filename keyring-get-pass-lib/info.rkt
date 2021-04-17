489b34f3bf9d5980c9b192772167b5e41aa2275b
Sam Phillips <samdphillips@gmail.com>
2021-04-17 10:09:45 -0700

    Flatten get-pass package structure.

--- keyring-get-pass-lib/info.rkt
+++ keyring-get-pass-lib/info.rkt
@@ -1,8 +1,8 @@
 #lang info
 
-(define collection 'multi)
 (define name "keyring-get-pass-lib")
-(define version 0.9)
+(define collection "keyring")
+(define version "0.10")
 (define deps '("base" "get-pass" "keyring-lib"))
 (define build-deps '("base" "rackunit-lib"))
 (define pkg-authors '(samdphillips@gmail.com))
