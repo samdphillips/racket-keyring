da3896c1c0e080413caf7b6b823e2cc84a4a8d23
Sam Phillips <samdphillips@gmail.com>
2021-04-17 10:45:34 -0700

    Flatten secret-service package

--- keyring-secret-service-lib/info.rkt
+++ keyring-secret-service-lib/info.rkt
@@ -1,8 +1,9 @@
 #lang info
 
-(define collection 'multi)
 (define name "keyring-secret-service-lib")
-(define version 0.9)
+(define collection "keyring")
+(define version "0.10")
 (define deps '("base" "dbus" "keyring-lib"))
 (define build-deps '("base" "rackunit-lib"))
 (define pkg-authors '(samdphillips@gmail.com))
+
