54d9360cdea2ffaa498d836165125c3f4786aabc
Sam Phillips <samdphillips@gmail.com>
2023-11-02 19:41:48 -0700

    Add dbus dep for keyring-secret-service-test

--- keyring-secret-service-test/info.rkt
+++ keyring-secret-service-test/info.rkt
@@ -4,6 +4,6 @@
 (define collection "keyring")
 (define version "0.11")
 (define deps '("base" "keyring-secret-service-lib"))
-(define build-deps '("base" "rackunit-lib"))
+(define build-deps '("base" "dbus" "rackunit-lib"))
 (define pkg-authors '(samdphillips@gmail.com))
 (define license 'Apache-2.0)
